5f255cc429e39c69219777671ca547533a383cf7
patik1242 <pkustra114@interia.pl>
2026-02-17 10:03:34 +0100

    LAST CHANGES

---
+++ app.py
@@ -0,0 +1,30 @@
+import app, pickle
+from preprocessing import preprocess_vector
+
+app = app.Flask(__name__, template_folder='templates')
+vectorizer = None 
+
+with open("models/model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+with open("models/vectorizer.pkl", "rb") as f:
+    vectorizer = pickle.load(f) 
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    sentiment = None
+
+    if app.request.method =="POST":
+        text = app.request.form["sentiment"]
+        clean_text = preprocess_vector(text)
+        
+        X_vec = vectorizer.transform([clean_text])
+
+        pred = model.predict(X_vec)
+
+        sentiment = "Positive" if pred[0]==1 else "Negative"
+
+    return app.render_template("index.html", sentiment=sentiment)
+
+if __name__ == "__main__":
+    app.run(debug=True)
